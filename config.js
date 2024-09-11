2cfb14628e08d3b753c9bbc65db601f4fc24b890
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-12 02:14:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID = "PbZ1lagZ#B08Hq5TTxUPS34XYHxDYkE5qFcbgrxjR4an1S3mn_nk",
-MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
+//SESSION_ID: process.env.SESSION_ID = "PbZ1lagZ#B08Hq5TTxUPS34XYHxDYkE5qFcbgrxjR4an1S3mn_nk",
+//MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
+SESSION_ID: process.env.SESSION_ID = "2D4AULiR#4klG80psrnemihrBhZXwjvf4EGroeEccXVLvqQn0BcU",
+MONGODB: process.env.MONGODB || "mongodb://mongo:remMADEnKcXlyPWwkUoNqqWfyRuqMAoF@junction.proxy.rlwy.net:34095",
 };
