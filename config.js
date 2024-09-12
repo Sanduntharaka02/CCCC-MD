2569d57f50987859e6b52b0bc85bf76ef541b411
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-12 18:13:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-//SESSION_ID: process.env.SESSION_ID = "PbZ1lagZ#B08Hq5TTxUPS34XYHxDYkE5qFcbgrxjR4an1S3mn_nk",
-//MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID = "PbZ1lagZ#B08Hq5TTxUPS34XYHxDYkE5qFcbgrxjR4an1S3mn_nk",
+MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
-SESSION_ID: process.env.SESSSION_ID = "SVtQ0TDL#HQCwOZNI-J_eL_niKYKxtfxWjLHR4uBo6C_O_wcurh4",
-MONGODB: process.env.MONGODB || "mongodb://mongo:remMADEnKcXlyPWwkUoNqqWfyRuqMAoF@junction.proxy.rlwy.net:34095",
+//SESSION_ID: process.env.SESSSION_ID = "SVtQ0TDL#HQCwOZNI-J_eL_niKYKxtfxWjLHR4uBo6C_O_wcurh4",
+//MONGODB: process.env.MONGODB || "mongodb://mongo:remMADEnKcXlyPWwkUoNqqWfyRuqMAoF@junction.proxy.rlwy.net:34095",
 };
