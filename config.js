ae7d4529f12f6973fe7a90dd51681633ed5431e2
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-14 01:02:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID = "WJ9wGDia#4a16XLVzFBi-sjI_03hTSjbKq2aWhz8cbNUyAAjobvI",
+SESSION_ID: process.env.SESSION_ID = "qb4xWbbL#0ezg41-XQ-73bwui3LHqxyC-fbmRcg0xsiuJj-BlvvA",
 MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
 //SESSION_ID: process.env.SESSSION_ID = "VtoDgZ6I#XW2NfCKlR43Is3MgHNcLWs7TszoADRFyCheXL1Zd1uY",
