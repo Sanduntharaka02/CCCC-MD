84573c195a0615a49f81e4b2ebe359fc95ad4853
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-13 07:36:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID = "VtoDgZ6I#XW2NfCKlR43Is3MgHNcLWs7TszoADRFyCheXL1Zd1uY",
 MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
-//SESSION_ID: process.env.SESSSION_ID = "UDkHybZZ#da9UbJsCodHfl_H_G_HW7yIPpczaFTw7cJthwG46xBQ",
+//SESSION_ID: process.env.SESSSION_ID = "WJ9wGDia#4a16XLVzFBi-sjI_03hTSjbKq2aWhz8cbNUyAAjobvI",
 //MONGODB: process.env.MONGODB || "mongodb://mongo:remMADEnKcXlyPWwkUoNqqWfyRuqMAoF@junction.proxy.rlwy.net:34095",
 };
