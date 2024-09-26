e1a0b288a93fae6f9675e9ad0520d478ee464923
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-27 01:59:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
 //MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
 SESSION_ID: process.env.SESSION_ID = "cPNE0bQC#YShImOimxcCB_KXc6NS7eV2b4CqrMGd2TU_Lq2jpoJo",
-MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-3:9ncIr7T3GcpbYX71@cluster0.1jh1y.mongodb.net/",
+MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-3:qwertyuiop@cluster0.1jh1y.mongodb.net/",
     
 //SESSION_ID: process.env.SESSSION_ID = "vLJxiaAL#OpgK5yGhCEFXrWAVc2HxtNIn5P8wTUezTSw_vuQ2pDw",
 //MONGODB: process.env.MONGODB || "mongodb+srv://tsandun:246810@cluster0.hpsxf.mongodb.net/",
