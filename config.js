27048dc68866f7ecc11b63aa426af26c69dfcd1d
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-27 10:57:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,10 +14,6 @@ MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-3:LbomQ0vPu1p7Ux1M@c
 //SESSION_ID: process.env.SESSSION_ID = "vLJxiaAL#OpgK5yGhCEFXrWAVc2HxtNIn5P8wTUezTSw_vuQ2pDw",
 //MONGODB: process.env.MONGODB || "mongodb+srv://tsandun:246810@cluster0.hpsxf.mongodb.net/",
 
-//SESSION_ID: process.env.SESSSION_ID = "1mdy2bJJ#B4jRte3KJxanMjeMrw7bJmjYGHbh55K-Cj6BnObaOpY",
-//MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-2:0iPJ9OqtWQfBNlYN@cluster0.rnd3h.mongodb.net/",
-
-
 };
 
 //vUkFkQaS#fYllxjZEjIFvvGPoLFO-6qA4ZEu3PK7xWrkexo1qikU
