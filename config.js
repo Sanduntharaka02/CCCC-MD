cd6e74e110799d461a6afbd8c7bd27716e96e59f
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-28 09:50:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,14 @@ module.exports = {
 //SESSION_ID: process.env.SESSION_ID = "4fNSFSoA#ijTn6dGYv3LcwnYIixdb2FeA_kwj-0R8NkAGGC5Ee5E",
 //MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 
-SESSION_ID: process.env.SESSION_ID = "cPNE0bQC#YShImOimxcCB_KXc6NS7eV2b4CqrMGd2TU_Lq2jpoJo",
-MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-3:LbomQ0vPu1p7Ux1M@cluster0.1jh1y.mongodb.net/",
+//SESSION_ID: process.env.SESSION_ID = "cPNE0bQC#YShImOimxcCB_KXc6NS7eV2b4CqrMGd2TU_Lq2jpoJo",
+//MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-3:LbomQ0vPu1p7Ux1M@cluster0.1jh1y.mongodb.net/",
     
 //SESSION_ID: process.env.SESSSION_ID = "vLJxiaAL#OpgK5yGhCEFXrWAVc2HxtNIn5P8wTUezTSw_vuQ2pDw",
 //MONGODB: process.env.MONGODB || "mongodb+srv://tsandun:246810@cluster0.hpsxf.mongodb.net/",
 
+SESSION_ID: process.env.SESSSION_ID = "c7tA3S4R#KdXRnSlGrtov2zKceQRNgEyzH2Va32z9mSuKMzs3Xvo",
+MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898:qlmYRWrI9brXoDdJ@cluster0.f8a9jgn.mongodb.net/",
 };
 
 //vUkFkQaS#fYllxjZEjIFvvGPoLFO-6qA4ZEu3PK7xWrkexo1qikU
