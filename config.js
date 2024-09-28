24b2404e160693e8f7c2790149420c79303ca887
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-09-28 15:12:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,9 @@ module.exports = {
 //SESSION_ID: process.env.SESSSION_ID = "vLJxiaAL#OpgK5yGhCEFXrWAVc2HxtNIn5P8wTUezTSw_vuQ2pDw",
 //MONGODB: process.env.MONGODB || "mongodb+srv://tsandun:246810@cluster0.hpsxf.mongodb.net/",
 
-SESSION_ID: process.env.SESSSION_ID = "c7tA3S4R#KdXRnSlGrtov2zKceQRNgEyzH2Va32z9mSuKMzs3Xvo",
+SESSION_ID: process.env.SESSSION_ID = "1mdy2bJJ#B4jRte3KJxanMjeMrw7bJmjYGHbh55K-Cj6BnObaOpY",
 MONGODB: process.env.MONGODB || "mongodb+srv://mrck-md-6898-2:0iPJ9OqtWQfBNlYN@cluster0.rnd3h.mongodb.net/",
 };
 
 //vUkFkQaS#fYllxjZEjIFvvGPoLFO-6qA4ZEu3PK7xWrkexo1qikU
+//c7tA3S4R#KdXRnSlGrtov2zKceQRNgEyzH2Va32z9mSuKMzs3Xvo
